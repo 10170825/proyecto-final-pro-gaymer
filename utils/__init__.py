10786b8927e73bbac7baac6c8d28968965c94ad0
jessica <jessymontano@hotmail.com>
2024-04-19 20:51:22 -0700

    exportar funcion get_id_journal_dict

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
 from .scraper import paginate_and_write_to_csv
-from .utils import get_abc_words_dict, get_word_journals_dict
+from .utils import get_abc_words_dict, get_word_journals_dict, get_id_journal_dict
